76ee7425a1f3d9caa1b8a0f8b4561c81285d446e
joshi232004manish <manishjoshi232004@gmail.com>
2025-04-10 07:00:32 +0530

    correction 1

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Github-User-Analyzer/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
